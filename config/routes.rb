23efa8dbd695e000e93fe4a4c0b8cc40e1e701fd
Gabriel Packer <ggkpacker@gmail.com>
2018-07-20 08:17:30 -0300

    Add RESTful routes to posts and root to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
+
+  get 'pages/home'
+
+  root to: 'pages#home'
 end
